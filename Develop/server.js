88b679a6abbebf16b0c81e12ba53869bb517c154
PriMac <prikshit_chauhan@bonify.de>
2022-08-23 23:19:15 +0200

    update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs')
+
+// npm package that allows for unique ids to be created
+var uniqid = require('uniqid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,6 +18,44 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './db/db.json'));
+});
+
+// POST /api/notes should receive a new note to save on the request body, 
+// add it to the db.json file, and then return the new note to the client. 
+app.post('/api/notes', (req, res) => {
+  let db = fs.readFileSync('./db/db.json');
+  db = JSON.parse(db);
+  res.json(db);
+  // creating body for note
+  let userNote = {
+    title: req.body.title,
+    text: req.body.text,
+    // creating unique id for each note
+    id: uniqid(),
+  };
+  // pushing created note to be written in the db.json file
+  db.push(userNote);
+  fs.writeFileSync('./db/db.json', JSON.stringify(db));
+  res.json(db);
+
+});
+
+
+// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete.
+app.delete('/api/notes/:id', (req, res) => {
+  // reading notes form db.json
+  let db = JSON.parse(fs.readFileSync('./db/db.json'))
+  // removing note with id
+  let deleteNotes = db.filter(item => item.id !== req.params.id);
+  // Rewriting note to db.json
+  fs.writeFileSync('./db/db.json', JSON.stringify(deleteNotes));
+  res.json(deleteNotes);
+  
+})
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT} 🚀`)
